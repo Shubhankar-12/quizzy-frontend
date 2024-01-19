c9f42d42cb9291415f48df9f4bd8f72c954c3d83
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2024-01-20 01:19:07 +0530

    responsiveness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/logo.png'
 import { Link, Outlet, useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
+import { Squash as Hamburger } from 'hamburger-react'
 import { toast } from 'react-toastify';
 import { useLogoutMutation } from '../../slices/usersApiSlice';
 import { logout } from '../../slices/authSlice';
@@ -12,6 +13,7 @@ const Navbar = () => {
     const navigate = useNavigate();
     const { userInfo } = useSelector((state) => state.auth);
     const [logoutApiCall] = useLogoutMutation();
+    const [isToggled, setIsToggled] = useState(false);
 
     const logoutHandler = async () => {
         try {
@@ -53,6 +55,30 @@ const Navbar = () => {
                             )}
                     </ul>
                 </div>
+                <div className="nav-ham">
+                    <Hamburger toggled={isToggled} toggle={setIsToggled} color="#012ab7" />
+                </div>
+            </div>
+            <div className="nav-dropdown" style={isToggled ? { height: 'block' } : { display: 'none' }}>
+                <ul className="nav-list">
+                    {userInfo ?
+                        (<>
+                            <Link to='/profile'> <li className="login-btn">
+                                {userInfo?.name?.split(' ')[0]}
+                            </li>
+                            </Link>
+                            <li className="signup-btn" onClick={logoutHandler}>Log out</li>
+                        </>)
+                        :
+                        (<>
+                            <Link to='/login'> <li className="login-btn">
+                                Login
+                            </li>
+                            </Link>
+                            <Link to='/signup'><li className="signup-btn">Sign Up</li></Link>
+                        </>
+                        )}
+                </ul>
             </div>
             <Outlet />
         </>
